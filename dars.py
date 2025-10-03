2c02a4e90a012eefc28b39515f6064984e2a2bae
Your Name <jamshidbek200803@example.com>
2025-10-03 16:46:11 +0500

    maktab sayti

---
+++ dars.py
@@ -0,0 +1,102 @@
+# 12.
+# son=int(input("son kiriting"))
+# if son> 10 and son<20:
+#  print("10dan katta va 20dan kichik raqam:")
+# else:
+#   print("10dan kichik raqam")
+#   print("10dan katta 20dan kichik raqam")
+  
+# 13.
+# son = int(input("Son kiriting: "))
+# if son % 3 == 0 and son % 5 == 0:
+#     print("Bu son 3 ga ham 5 ga ham bo'linadi.")
+# else:
+#     print("Bu son 3 ga ham 5 ga ham bo'linmaydi.")
+
+14
+# a = int(input("Birinchi sonni kiriting: "))
+# b = int(input("Ikkinchi sonni kiriting: "))
+# if a > b:
+#     print("Birinchi son katta")
+# elif b > a:
+#     print("Ikkinchi son katta")
+# else:
+#     print("Ikkala son teng")
+
+# 15.
+# import math
+
+# def is_prime(n: int) -> bool:
+#     """n butun soni tub (prime) bo'lsa True, aks holda False."""
+#     if n <= 1:
+#         return False
+#     if n <= 3:
+#         return True       
+#     if n % 2 == 0 or n % 3 == 0:
+#         return False
+#     limit = int(math.isqrt(n))
+#     i = 5
+#     while i <= limit:
+#         if n % i == 0 or n % (i + 2) == 0:
+#             return False
+#         i += 6
+#     return True
+
+# def main():
+#         s = input("Butun son kiriting: ").strip()
+#         n = int(s)
+
+16 
+# n = float(input("Son kiriting: "))
+
+# if n < 0:
+#     print("manfiy")
+# else:
+#     print("musbat")
+
+17.
+# soz=input("matn kiriting:")
+# if soz.startswith("payton"):
+#     print("phyton sozi bilan boshlangan ")
+# else:
+#     print("soz phytn bilan boshlanmagan")
+
+18.
+# soz=input("matn kiriting:").upper()
+# print(soz.lower())
+
+# 1
+# son=int(input("son kiriting"))
+# if son<0:
+#     print("manfiy son ")
+# else:
+#     print("aks holda musbat son")
+
+2
+# son=int(input("son kiriting"))
+
+
+
+
+# telefon=input("nomeringizni kiriting:")
+# if telefon.startswith("+998"):
+#     print("uzbga tegishli:")
+# elif telefon.startswith("+779"):
+#     print("rusga tegishli:")
+# elif telefon.startswith("+92"):
+#     print("koreaga tegishli:")
+# elif telefon.startswith("+90"):
+#     print("turkeaga tegishli:")
+
+# fayl=input("fayl kiriting:")
+# if fayl.endswith(".jpg"):
+#     print("androidga tegishli:")
+# elif fayl.endswith(".dng"):
+#     print("iphonega tegishli:")
+
+# son=False
+# if (3>5 or 4>3) and 1<3:
+#     son=True
+#     print(son)
+
+dugonalarim["Sabina,Xadicha,Fotima:"]
